227a45c02d08f4e18413e38b31a7b7c75909aa6a
Trent Willis <trentmwillis@gmail.com>
2019-08-12 14:21:36 -0700

    Refactor generated-background to use ezOffscreenCanvas for rendering

--- assets/generated-background-element.js
+++ assets/generated-background-element.js
@@ -7,12 +7,6 @@ class GeneratedBackground extends HTMLElement {
         const shadow = this.attachShadow({ mode: 'closed' });
         this.canvas = document.createElement('canvas');
 
-        this.resizer = () => {
-            const boundingRect = this.parentElement.getBoundingClientRect();
-            this.canvas.width = boundingRect.width;
-            this.canvas.height = boundingRect.height;
-        };
-
         shadow.innerHTML = `
         <style>
         :host {
@@ -26,43 +20,53 @@ class GeneratedBackground extends HTMLElement {
     }
 
     connectedCallback() {
+        this.worker = this.render();
+        this.resizer = () => {
+            const boundingRect = this.parentElement.getBoundingClientRect();
+            this.worker.setAttributes({
+                width: boundingRect.width,
+                height: boundingRect.height,
+            });
+        };
         window.addEventListener('resize', this.resizer, { passive: true });
         this.resizer();
-        this.render();
     }
 
     disconnectedCallback() {
         window.removeEventListener('resize', this.resizer, { passive: true });
+        this.worker.terminate();
     }
 
     render() {
         const canvas = this.canvas;
-        const context = canvas.getContext('2d');
+        return ezOffscreenCanvas(canvas, null, ({ canvas }) => {
+            const context = canvas.getContext('2d');
 
-        let shift = 0;
+            let shift = 0;
 
-        const draw = () => {
-            const width = canvas.width;
-            const height = canvas.height;
-            const maxAmplitude = height / 2;
-            const frequency = 1 / 200;
+            const draw = () => {
+                const width = canvas.width;
+                const height = canvas.height;
+                const maxAmplitude = height / 2;
+                const frequency = 1 / 200;
 
-            context.clearRect(0, 0, width, height);
-            context.fillStyle = 'rgba(255, 255, 255, 0.75)';
+                context.clearRect(0, 0, width, height);
+                context.fillStyle = 'rgba(255, 255, 255, 0.75)';
 
-            for (let x = 0; x < width; x += 10) {
-                for (let amplitude = 40; amplitude < maxAmplitude; amplitude += 20) {
-                    const y = Math.sin((shift - x) * frequency) * amplitude;
-                    context.fillRect(x, (height / 2) + y, 1, 1);
+                for (let x = 0; x < width; x += 10) {
+                    for (let amplitude = 40; amplitude < maxAmplitude; amplitude += 20) {
+                        const y = Math.sin((shift - x) * frequency) * amplitude;
+                        context.fillRect(x, (height / 2) + y, 1, 1);
+                    }
                 }
+
+                shift = shift * frequency > Math.PI * 2 ? 0 : shift + 1;
+
+                requestAnimationFrame(draw);
             }
 
-            shift = shift * frequency > Math.PI * 2 ? 0 : shift + 1;
-
-            requestAnimationFrame(draw);
-        }
-
-        draw();
+            draw();
+        });
     }
 }
 
